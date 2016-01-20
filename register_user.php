4b682fb0684e65ea603b25c8dd51372a99983cfb
ebickerdyke <firework2117@gmail.com>
2016-01-20 16:21:23 +0000

    Update register_user.php

--- register_user.php
+++ register_user.php
@@ -1,9 +1,21 @@
 <?php
-	
+	//import functions
+	require 'functions.php';
+
 	//Connect to DB, commit sudoku otherwise
 	$dbconnection = mysqli_connect($host, $user, $password, $dbname);
 	if(!$dbconnection){
 		die("Error in query " . mysqli_error($dbconnection));
 	}
 
+	//get data from form
+	$firstname = $_POST['firstname'];
+	$surname = $_POST['surname'];
+	$addr1 = $_POST['addr1'];
+	$addr2 = $_POST['addr2'];
+	$addr3 = $_POST['addr3'];
+	$county = $_POST['county'];
+
+	//call insert function
+	insertIntoUsersTable($firstname, $surname, $addr1, $addr2, $addr3, $county);
 ?>
