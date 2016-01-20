6c2ac9f4af8bbb3497942b3b93992a56c6cac537
ebickerdyke <firework2117@gmail.com>
2016-01-20 16:00:06 +0000

    function insertIntoUsersTable is added

--- functions.php
+++ functions.php
@@ -1,1 +1,17 @@
 <-- Add functions here for use throughout project -->
+
+	function insertIntoUsersTable (&$firstName, &$surname, &$addr1, &$addr2, &$addr3, &$county){
+		if (!$addr3) {
+			$query = "INSERT INTO users_table (firstname, surname, addr1, addr2, county)
+						VALUES ('{$firstname}', '{$surname}', '{$addr1}', '{$addr2}', '{$county}');";
+		}
+		else {
+			$query = "INSERT INTO users_table (firstname, surname, addr1, addr2, county)
+						VALUES ('{$firstname}', '{$surname}', '{$addr1}', '{$addr2}', '{addr3}', '{$county}');";
+		}
+		
+		$db_insert = mysqli_query($dbconnection, $query);
+		if (!$db_insert) {
+			die();
+		}
+	}
