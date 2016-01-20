f1c51181e5d87b1c2a40a4ef29f034cf5d8dbc5e
TheSlapDoctor <bobby.echo13@gmail.com>
2016-01-20 15:51:08 +0000

    Delete register_user.php

--- register_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	//Connect to DB, commit sudoku otherwise
-	$dbconnection = mysqli_connect($host, $user, $password, $dbname);
-	if(!$dbconnection){
-		die("Error in query " . mysqli_error($dbconnection));
-	}
-
-?>
