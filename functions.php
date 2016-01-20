5ad3a045ab2b9c0a5921f7f63bce534bbbec2db9
ebickerdyke <firework2117@gmail.com>
2016-01-20 16:39:32 +0000

    Email variable added

--- functions.php
+++ functions.php
@@ -1,17 +1,17 @@
 <-- Add functions here for use throughout project -->
 <?php
 	//Designed to take data and insert into the users table of the database
-	function insertIntoUsersTable (&$firstName, &$surname, &$addr1, &$addr2, &$addr3, &$county){
-		//acertain if optional data is present
+	function insertIntoUsersTable (&$firstName, &$surname, &$email, &$addr1, &$addr2, &$addr3, &$county){
+		//acertain if optional data is present, probably don't need this but can't test yet
 		if (!$addr3) {
 			//define SQL query w/ optional data
-			$query = "INSERT INTO users_table (firstname, surname, addr1, addr2, county)
-						VALUES ('{$firstname}', '{$surname}', '{$addr1}', '{$addr2}', '{$county}');";
+			$query = "INSERT INTO users_table (firstname, surname, email, addr1, addr2, county)
+						VALUES ('{$firstname}', '{$surname}','{$email}', '{$addr1}', '{$addr2}', '{$county}');";
 		}
 		else {
 			//define SQL query w/o optional data 
-			$query = "INSERT INTO users_table (firstname, surname, addr1, addr2, county)
-						VALUES ('{$firstname}', '{$surname}', '{$addr1}', '{$addr2}', '{addr3}', '{$county}');";
+			$query = "INSERT INTO users_table (firstname, surname, email, addr1, addr2, county)
+						VALUES ('{$firstname}', '{$surname}', '{$email}', '{$addr1}', '{$addr2}', '{$addr3}', '{$county}');";
 		}
 		
 		//make insert query, become an hero otherwise
